e2d28df82b3aceea1df1cdba98c9b4e06a5bbc5d
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2024-01-23 13:17:03 +0000

    added listindex support for targeting

--- SomethingNeedDoing/Grammar/Commands/TargetCommand.cs
+++ SomethingNeedDoing/Grammar/Commands/TargetCommand.cs
@@ -18,6 +18,7 @@ internal class TargetCommand : MacroCommand
 
     private readonly string targetName;
     private readonly int targetIndex;
+    private readonly int listIndex;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="TargetCommand"/> class.
@@ -26,11 +27,12 @@ internal class TargetCommand : MacroCommand
     /// <param name="targetName">Target name.</param>
     /// <param name="wait">Wait value.</param>
     /// <param name="index">Object index.</param>
-    private TargetCommand(string text, string targetName, WaitModifier wait, IndexModifier index)
+    private TargetCommand(string text, string targetName, WaitModifier wait, IndexModifier index, ListIndexModifier listIndex)
         : base(text, wait, index)
     {
         this.targetIndex = index.ObjectId;
         this.targetName = targetName.ToLowerInvariant();
+        this.listIndex = listIndex.ListIndex;
     }
 
     /// <summary>
@@ -42,12 +44,13 @@ internal class TargetCommand : MacroCommand
     {
         _ = WaitModifier.TryParse(ref text, out var waitModifier);
         _ = IndexModifier.TryParse(ref text, out var indexModifier);
+        _ = ListIndexModifier.TryParse(ref text, out var listIndexModifier);
         var match = Regex.Match(text);
         if (!match.Success)
             throw new MacroSyntaxError(text);
 
         var nameValue = ExtractAndUnquote(match, "name");
-        return new TargetCommand(text, nameValue, waitModifier, indexModifier);
+        return new TargetCommand(text, nameValue, waitModifier, indexModifier, listIndexModifier);
     }
 
     /// <inheritdoc/>
@@ -56,7 +59,10 @@ internal class TargetCommand : MacroCommand
         Service.Log.Debug($"Executing: {this.targetIndex}");
         var target = Service.ObjectTable
             .OrderBy(o => Vector3.Distance(o.Position, Service.ClientState.LocalPlayer!.Position))
-            .FirstOrDefault(obj => obj.Name.TextValue.ToLowerInvariant() == this.targetName && obj.IsTargetable && (this.targetIndex <= 0 || obj.ObjectIndex == this.targetIndex));
+            .Where(obj => obj.Name.TextValue.ToLowerInvariant() == this.targetName && obj.IsTargetable && (this.targetIndex <= 0 || obj.ObjectIndex == this.targetIndex))
+            .Skip(this.listIndex)
+            .FirstOrDefault();
+
 
         if (target == default && Service.Configuration.StopMacroIfTargetNotFound)
             throw new MacroCommandError("Could not find target");
---
+++ SomethingNeedDoing/Grammar/Modifiers/ListIndexModifier.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace SomethingNeedDoing.Grammar.Modifiers;
+
+/// <summary>
+/// The &lt;list index&gt; modifier.
+/// </summary>
+internal class ListIndexModifier : MacroModifier
+{
+    private static readonly Regex Regex = new(@"(?<modifier><list\.(?<listIndex>\d+(?:\.\d+)?)>)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+    private ListIndexModifier(int listIndex)
+    {
+        this.ListIndex = listIndex;
+    }
+
+    public int ListIndex { get; }
+
+    /// <summary>
+    /// Parse the text as a modifier.
+    /// </summary>
+    /// <param name="text">Text to parse.</param>
+    /// <param name="command">A parsed modifier.</param>
+    /// <returns>A value indicating whether the modifier matched.</returns>
+    public static bool TryParse(ref string text, out ListIndexModifier command)
+    {
+        var match = Regex.Match(text);
+        var success = match.Success;
+
+        if (!success)
+        {
+            command = new ListIndexModifier(0);
+            return false;
+        }
+
+        var group = match.Groups["modifier"];
+        text = text.Remove(group.Index, group.Length);
+
+        var indexGroup = match.Groups["listIndex"];
+        var indexValue = indexGroup.Value;
+        var index = int.Parse(indexValue, CultureInfo.InvariantCulture);
+
+        command = new ListIndexModifier(index);
+        return true;
+    }
+}
