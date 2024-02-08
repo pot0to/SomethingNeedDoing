3f39bec92650c624964b9403b8a976fc2e7954b7
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2024-02-08 10:59:49 +0000

    remove lb controller (again)

--- SomethingNeedDoing/Misc/ActiveMacro.cs
+++ SomethingNeedDoing/Misc/ActiveMacro.cs
@@ -314,7 +314,7 @@ internal partial class ActiveMacro : IDisposable
             this.lua["FateManager"] = (FateManager)Marshal.PtrToStructure((nint)FateManager.Instance(), typeof(FateManager));
             this.lua["Framework"] = (Framework)Marshal.PtrToStructure((nint)Framework.Instance(), typeof(Framework));
             this.lua["InventoryManager"] = (InventoryManager)Marshal.PtrToStructure((nint)InventoryManager.Instance(), typeof(InventoryManager));
-            this.lua["LimitBreakController"] = (LimitBreakController)Marshal.PtrToStructure((nint)LimitBreakController.Instance(), typeof(LimitBreakController));
+            //this.lua["LimitBreakController"] = (LimitBreakController)Marshal.PtrToStructure((nint)LimitBreakController.Instance(), typeof(LimitBreakController));
             this.lua["PlayerState"] = (PlayerState)Marshal.PtrToStructure((nint)PlayerState.Instance(), typeof(PlayerState));
             this.lua["QuestManager"] = (QuestManager)Marshal.PtrToStructure((nint)QuestManager.Instance(), typeof(QuestManager));
             this.lua["RouletteController"] = (RouletteController)Marshal.PtrToStructure((nint)RouletteController.Instance(), typeof(RouletteController));
