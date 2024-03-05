6063012948e5880c54b2bcf07b6babce74c48c4e
stjornur <162118988+stjornur@users.noreply.github.com>
2024-03-05 17:28:05 +0000

    improve require(), allow requiring files as well as macros

--- SomethingNeedDoing/Misc/Commands/InternalCommands.cs
+++ SomethingNeedDoing/Misc/Commands/InternalCommands.cs
@@ -7,12 +7,13 @@ public class InternalCommands
 {
     internal static InternalCommands Instance { get; } = new();
 
-    public string InternalGetMacroText(string name)
+    public string? InternalGetMacroText(string name)
     {
         return Service.Configuration
             .GetAllNodes()
             .OfType<MacroNode>()
-            .First(node => string.Equals(node.Name.Trim(), name.Trim(), StringComparison.InvariantCultureIgnoreCase))
+            .FirstOrDefault(node =>
+                string.Equals(node.Name.Trim(), name.Trim(), StringComparison.InvariantCultureIgnoreCase))?
             .Contents
             .Split(["\r\n", "\r", "\n"], StringSplitOptions.None)
             .Select(line => $"  {line}")
