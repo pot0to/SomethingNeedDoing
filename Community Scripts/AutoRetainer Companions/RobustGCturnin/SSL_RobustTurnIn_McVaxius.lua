b47d5f771624a4419333bf8c2a7969862503b5a9
davoodinator <135515620+McVaxius@users.noreply.github.com>
2024-04-13 16:28:31 -0400

    oops changed the testing filename now it makes more sense for someone to read it
    oops changed the testing filename now it makes more sense for someone to read it

--- Community Scripts/AutoRetainer Companions/RobustGCturnin/SSL_RobustTurnIn_McVaxius.lua
+++ Community Scripts/AutoRetainer Companions/RobustGCturnin/SSL_RobustTurnIn_McVaxius.lua
@@ -2,7 +2,7 @@
 --purpose: edit the lua script in notepad++ instead of ingame because we dont want to use the ingame editor And we have a million clients running
 --does it change anything? no . it just simplifies editing multiple script instances locally for your use with multiple chars.
 --for robust turnin script
-loadfiyelZZ = os.getenv("appdata").."\\XIVLauncher\\pluginConfigs\\SomethingNeedDoing\\boobers.lua"
+loadfiyelZZ = os.getenv("appdata").."\\XIVLauncher\\pluginConfigs\\SomethingNeedDoing\\RobustGCTurnIn_McVaxius.lua"
 functionsToLoad = loadfile(loadfiyelZZ)
 functionsToLoad()
 DidWeLoadcorrectly()
