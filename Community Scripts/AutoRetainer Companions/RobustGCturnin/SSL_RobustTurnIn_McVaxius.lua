c0ee0eaaf257d988305087bf5cfb59998aa81a30
davoodinator <135515620+McVaxius@users.noreply.github.com>
2024-04-13 16:32:54 -0400

    .
    .

--- Community Scripts/AutoRetainer Companions/RobustGCturnin/SSL_RobustTurnIn_McVaxius.lua
+++ Community Scripts/AutoRetainer Companions/RobustGCturnin/SSL_RobustTurnIn_McVaxius.lua
@@ -1,6 +1,8 @@
 --Super Sneaky Loader (tm)
 --purpose: edit the lua script in notepad++ instead of ingame because we dont want to use the ingame editor And we have a million clients running
 --does it change anything? no . it just simplifies editing multiple script instances locally for your use with multiple chars.
+
+--thanks to eden from the disc for the idea!
 --for robust turnin script
 loadfiyelZZ = os.getenv("appdata").."\\XIVLauncher\\pluginConfigs\\SomethingNeedDoing\\RobustGCTurnIn_McVaxius.lua"
 functionsToLoad = loadfile(loadfiyelZZ)
