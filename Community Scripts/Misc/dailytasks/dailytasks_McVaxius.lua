a10b72f6a037bef3802662e587fea810e41d81b5
davoodinator <135515620+McVaxius@users.noreply.github.com>
2024-04-14 11:14:00 -0400

    its an idea for now
    its an idea for now

---
+++ Community Scripts/Misc/dailytasks/dailytasks_McVaxius.lua
@@ -0,0 +1,34 @@
+--[[
+What is this ?
+
+a kind of manager script that does various things based on certain rules, continually so you can just fire and forget it.
+
+you fire up the script and hope it doesnt die at some point and it will do the following things nonstop while your afk
+
+obviously it will return chars safely home to bell/fc entrance and resume autoretainer in between activities
+
+maybe this would be better as a plugin not as a script.
+
+******************************
+Stuff I think is possible now
+******************************
+*check for certain FC buffs every 12 hours with a specific set of array of chars (in case you are managing multiple active FC)
+*run cuff-a-cur for x hours a day at a specific time y on specific char with a base64 visland route to get to the machine
+*run HOH intuition farm for x hours a day on a specific char
+*run fishing every 2 hours on a rotating list of chars
+
+************************************************************
+I'm not sure if its possible or needs some slash commands?
+************************************************************
+*run TT for x3 hours a day at a specific time y on a specific char
+*attempts to buys 5 stack of fuel @ any FC that is below x value. checks every 12 hours
+
+****************************************************************************************************
+Stuff that would be nice but technically annoying to do since we need more stuff for it to "happen"
+****************************************************************************************************
+*visit the personal island once a week and somehow do the island stuff and then leave
+*play mini cactpot daily on a list of chars at y time
+*play jumbo cactpot weekly on a list of chars at y time
+*play verminion weekly on a list of chars at y time
+
+]]
