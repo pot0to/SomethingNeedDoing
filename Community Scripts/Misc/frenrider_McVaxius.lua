051a1760d8f04b25fcb1d65816b9165790f18043
davoodinator <135515620+McVaxius@users.noreply.github.com>
2024-05-03 20:27:33 -0400

    improved frenrider
    improved frenrider

--- Community Scripts/Misc/frenrider_McVaxius.lua
+++ Community Scripts/Misc/frenrider_McVaxius.lua
@@ -12,16 +12,24 @@ visland
 bring some gysahl greens
 ]]
 
-fren = "Frend Name" --can be partial as long as its unique
-weirdvar = 1
+---------CONFIGURATION SECTION---------
+fren = "Frend Name" 	--can be partial as long as its unique
+cling = 0.5 			--distance to cling to fren
+clingy = true			--are we clingy? if not then the fren will have to swoop by to pick them up. recommend ON unless your doing quests or something.
 --mker = "cross" --in case you want the other shapes. valid shapes are triangle square circle attack1-8 bind1-3 ignore1-2
+-----------CONFIGURATION END-----------
 
 --init
 yield("/target \""..fren.."\"")
 yield("/wait 0.5")
 --yield("/mk cross <t>")
-local partycardinality = 2
 
+local function distance(x1, y1, z1, x2, y2, z2)
+    return math.sqrt((x2 - x1)^2 + (y2 - y1)^2 + (z2 - z1)^2)
+end
+
+weirdvar = 1
+local partycardinality = 2
 for i=0,7 do
 	if GetPartyMemberName(i) == fren then
 		partycardinality = i
@@ -43,21 +51,26 @@ while weirdvar == 1 do
 						yield("/wait 2")
 					end
 					--yield("/target <cross>")
-					yield("/target \""..fren.."\"")
+					if clingy then
+						--check distance to fren, if its more than cling, then
+						if distance(GetPlayerRawXPos(), GetPlayerRawYPos(), GetPlayerRawZPos(), GetObjectRawXPos(fren),GetObjectRawYPos(fren),GetObjectRawZPos(fren)) >  cling then
+						--yield("/target \""..fren.."\"")
+							PathfindAndMoveTo(GetObjectRawXPos(fren),GetObjectRawYPos(fren),GetObjectRawZPos(fren), false)
+						end
 						yield("/wait 0.5")
-						PathfindAndMoveTo(GetObjectRawXPos(fren),GetObjectRawYPos(fren),GetObjectRawZPos(fren), false)
-						--yield("/lockon on")
+					end	
+					--yield("/lockon on")
 					--yield("/automove on")
-					
+
 					--[[yield("/ridepillion <"..mker.."> 1")
 					yield("/ridepillion <"..mker.."> 2")
 					yield("/ridepillion <"..mker.."> 3")]]
 					if IsPartyMemberMounted(partycardinality) == true then
 						--for i=1,7 do
 							--yield("/ridepillion <"..partycardinality.."> "..i)
-							yield("/ridepillion <2> "..2)
-							yield("/ridepillion <3> "..2)
-							yield("/ridepillion <4> "..2)
+							yield("/ridepillion <2> 2")
+							yield("/ridepillion <3> 2")
+							yield("/ridepillion <4> 2")
 						--end
 						yield("/echo Attempting to Mount Friend")
 						yield("/wait 0.5")
