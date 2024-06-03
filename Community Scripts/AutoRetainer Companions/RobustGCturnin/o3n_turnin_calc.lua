1efa4bca0555d1bc13c4cb7997c0ca71223c019d
davoodinator <135515620+McVaxius@users.noreply.github.com>
2024-06-02 21:27:15 -0400

    smal var update in a4n. o3n pricer too
    smal var update in a4n. o3n pricer too

--- Community Scripts/AutoRetainer Companions/RobustGCturnin/o3n_turnin_calc.lua
+++ Community Scripts/AutoRetainer Companions/RobustGCturnin/o3n_turnin_calc.lua
@@ -1,4 +1,4 @@
---a4n turnincalculator
+--o3n turnincalculator
 --pointless calc and output
 
 function formatNumberWithCommas(number)
@@ -22,24 +22,25 @@ function formatNumberWithCommas(number)
     return formatted
 end
 
---a4n_head  = nyet
-a4n_hand  = math.floor(GetItemCount(12676) / 2)
-a4n_body  = math.floor(GetItemCount(12675) / 4)
-a4n_leg   = math.floor(GetItemCount(12677) / 4)
-a4n_feet  = math.floor(GetItemCount(12678) / 2)
-a4n_j     = math.floor(GetItemCount(12680))
-iLevel    = 190
-beegnumber = a4n_hand + a4n_body + a4n_leg + a4n_feet + a4n_j
+o3n_head  = math.floor(GetItemCount(19111) / 2) --lense
+o3n_hand  = math.floor(GetItemCount(19113) / 2) --crank
+o3n_body  = math.floor(GetItemCount(19112) / 4) --shaft
+o3n_leg   = math.floor(GetItemCount(19114) / 4) --spring
+o3n_feet  = math.floor(GetItemCount(19115) / 2) --pedal
+o3n_j     = math.floor(GetItemCount(19117))     --bold
+iLevel    = 320
+beegnumber = o3n_head + o3n_hand + o3n_body + o3n_leg + o3n_feet + o3n_j
 beegernumber = beegnumber * iLevel * 1.5
-gcbeegnumber = math.floor((beegnumber * 1093) / 600) * 360
-gcbeegnumber10 = math.floor((beegnumber * 1203) / 600) * 360
-gcbeegnumber15 = math.floor((beegnumber * 1257) / 600) * 360
+gcbeegnumber = math.floor((beegnumber * 1390) / 600) * 360
+gcbeegnumber10 = math.floor((beegnumber * 1529) / 600) * 360
+gcbeegnumber15 = math.floor((beegnumber * 1598) / 600) * 360
 
-yield("Total a4n_hand -> "..formatNumberWithCommas(a4n_hand))
-yield("Total a4n_body -> "..formatNumberWithCommas(a4n_body))
-yield("Total a4n_leg -> "..formatNumberWithCommas(a4n_leg))
-yield("Total a4n_feet -> "..formatNumberWithCommas(a4n_feet))
-yield("Total a4n_jewellery -> "..formatNumberWithCommas(a4n_j))
+yield("Total o3n_head -> "..formatNumberWithCommas(o3n_head))
+yield("Total o3n_hand -> "..formatNumberWithCommas(o3n_hand))
+yield("Total o3n_body -> "..formatNumberWithCommas(o3n_body))
+yield("Total o3n_leg -> "..formatNumberWithCommas(o3n_leg))
+yield("Total o3n_feet -> "..formatNumberWithCommas(o3n_feet))
+yield("Total o3n_jewellery -> "..formatNumberWithCommas(o3n_j))
 yield("Item level -> "..iLevel)
 yield("Total Turnins ->"..formatNumberWithCommas(beegnumber))
 yield("Total FC Points ->"..formatNumberWithCommas(beegernumber))
@@ -47,12 +48,16 @@ yield("Total Gil oilcloth  0% buff Value ->"..formatNumberWithCommas(gcbeegnumbe
 yield("Total Gil oilcloth 10% buff Value ->"..formatNumberWithCommas(gcbeegnumber10).." from "..formatNumberWithCommas(gcbeegnumber10 * (5/3)).." GC Seals")
 yield("Total Gil oilcloth 15% buff Value ->"..formatNumberWithCommas(gcbeegnumber15).." from "..formatNumberWithCommas(gcbeegnumber15 * (5/3)).." GC Seals")
 
-a4n_hand_gil = 587
-a4n_body_gil = 978
-a4n_leg_gil = 587
-a4n_feet_gil = 587
-a4n_j_gil = 445
+o3n_head_gil = 896
+o3n_hand_gil = 896
+o3n_body_gil = 1493
+o3n_leg_gil = 1493
+o3n_feet_gil = 896
+o3n_j_gil = 679
 
-total_vendor_gil = (a4n_hand * a4n_hand_gil) + (a4n_body * a4n_body_gil) + (a4n_leg * a4n_leg_gil) + (a4n_feet * a4n_feet_gil) + (a4n_j * a4n_j_gil)
+total_orch_scroll = GetItemCount(24297) * 150
+yield("/echo Total orch scrolls -> "..GetItemCount(24297).. ", Total gil -> "..total_orch_scroll)
+
+total_vendor_gil = (o3n_head * o3n_head_gil) + (o3n_hand * o3n_hand_gil) + (o3n_body * o3n_body_gil) + (o3n_leg * o3n_leg_gil) + (o3n_feet * o3n_feet_gil) + (o3n_j * o3n_j_gil)
 
 yield("Total Vendorsell (no GC/FC Ranking) Gil ->"..formatNumberWithCommas(total_vendor_gil))
