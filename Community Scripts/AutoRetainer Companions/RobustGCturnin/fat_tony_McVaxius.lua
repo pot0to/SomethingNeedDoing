8e2385b97d0f6f3cae067a87902dac0ab922c0c6
davoodinator <135515620+McVaxius@users.noreply.github.com>
2024-06-21 11:56:37 -0400

    prepping fat tony(ies) for receiving stuff
    prepping fat tony(ies) for receiving stuff

---
+++ Community Scripts/AutoRetainer Companions/RobustGCturnin/fat_tony_McVaxius.lua
@@ -0,0 +1,5 @@
+--[[
+Fat Tony script
+
+This is a pair script for the bagman script. basically it will load x chars go to location to wait for deliveries. when it receives a 1 gil trade, it knows its time to switch to next char.
+]]
