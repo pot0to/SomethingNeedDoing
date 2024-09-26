ae1db56413423d03f54e241722efcc3449f1e44e
davoodinator <135515620+McVaxius@users.noreply.github.com>
2024-09-26 09:59:26 -0400

    update to hacky aethryte unlock script
    update to hacky aethryte unlock script

--- Community Scripts/Quests And Levelling/Unlock Bronze Lake and Upper La Noscea/bronzenoscea_McVaaxius.lua
+++ Community Scripts/Quests And Levelling/Unlock Bronze Lake and Upper La Noscea/bronzenoscea_McVaaxius.lua
@@ -17,6 +17,7 @@ charlist = {
 for i=1,#charlist do
 	--load ze char
 	while charlist[i] ~= GetCharacterName(true) do
+		yield("/echo swithcing chars!")
 		yield("/ays relog " ..charlist[i])
 		yield("/wait 3")
 		yield("/waitaddon _ActionBar <maxwait.600><wait.5>")
@@ -28,6 +29,7 @@ for i=1,#charlist do
 		yield("/wait 3")
 	end
 
+	yield("/echo First chocobo mount")
 	yield("/mount \"Company Chocobo\"")
 	yield("/wait 6")
 	
@@ -35,16 +37,18 @@ for i=1,#charlist do
 	--wineport aetheryte is id 12
 
 	if IsAetheryteUnlocked(12) == true then
+		yield("/echo we have wineport unlocked so we tp to it")
 		yield("/tp wineport")
 		CharacterSafeWait()
 	end
 
 	if IsAetheryteUnlocked(12) == false then
 		--get to moraby :(
+		yield("/echo we dont have wineport unlocked so we need to go to moraby")
 		yield("/tp moraby")
 		CharacterSafeWait()
 
-		yield("/vnav "..movetype.." 572.73193359375 96.397186279297 -525.09948730469") --get to eastern las noscnea wineport side
+		yield("/vnav "..movetype.."to 572.73193359375 96.397186279297 -525.09948730469") --get to eastern las noscnea wineport side
 		visland_stop_moving()
 		CharacterSafeWait()
 		yield("/vnav "..movetype.."to -19.65797996521 70.50106048584 10.600910186768") --get to eastern las noscnea wineport side aetheryte
@@ -55,7 +59,8 @@ for i=1,#charlist do
 		yield("/wait 6")
 	end
 	
-	yield("/vnav "..movetype.." 80.876075744629 80.039474487305 -125.75521850586") --get to bronze lake
+	yield("/echo lets go to bronzeport")
+	yield("/vnav "..movetype.."to 80.876075744629 80.039474487305 -125.75521850586") --get to bronze lake
 	visland_stop_moving()
 	CharacterSafeWait()
 	yield("/vnav "..movetype.."to 434.98513793945 3.6090104579926 98.775543212891") --get to bronze lake aetheryte
@@ -65,7 +70,8 @@ for i=1,#charlist do
 	yield("/mount \"Company Chocobo\"")
 	yield("/wait 6")
 
-	yield("/vnav "..movetype.." 286.96008300781 41.385322570801 -203.57307434082") --get to upper la noscea
+	yield("/echo lets go to goblinport")
+	yield("/vnav "..movetype.."to 286.96008300781 41.385322570801 -203.57307434082") --get to upper la noscea
 	visland_stop_moving()
 	CharacterSafeWait()
 	yield("/vnav "..movetype.."to -117.79266357422 64.759223937988 -206.78842163086") --get to upper la noscea aetheryte
