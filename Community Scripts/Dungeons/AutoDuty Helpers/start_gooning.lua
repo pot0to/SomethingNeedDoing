20b8184ede2485480a3f46f980e177c693070e22
davoodinator <135515620+McVaxius@users.noreply.github.com>
2024-10-19 09:11:36 -0400

    we kicking the arcade machine real smart
    we kicking the arcade machine real smart

--- Community Scripts/Dungeons/AutoDuty Helpers/start_gooning.lua
+++ Community Scripts/Dungeons/AutoDuty Helpers/start_gooning.lua
@@ -1,3 +1,4 @@
+--this should be in the (Automaton) enhance duty/start end as /pcraft run start_gooning, obviously make the script in SND for this to work.
 --set this to run when entering a duty
 yield("/wait 2")
 --loop wait for is char ready 
@@ -6,7 +7,25 @@ while IsPlayerAvailable() == false do
 	yield("/wait 1")
 end
 yield("/ad stop")
+
+--optional part if your having issues with magitek skillis being spammed and interaction slow downs over time.
+--the following collection should contain pandora AND autoduty
+--this is not a safe solution and sometimes breaks the plugin when re enabling (REEEEEEEE enabling)
+yield("/xldisableprofile ad_collection")
+while HasPlugin("AutoDuty") or HasPlugin("Pandora")  do
+	yield("/wait 1")
+end
+yield("/wait 1")
+yield("/xlenableprofile ad_collection")
 yield("/wait 2")
 yield("/hold W <wait.2.0>")
 yield("/release W")
-yield("/ad start")
+--while not (HasPlugin("AutoDuty") and HasPlugin("Pandora")) do
+while not HasPlugin("AutoDuty") do
+	yield("/wait 1")
+	yield("/echo waiting on collection to turn back on")
+end
+	if HasPlugin("AutoDuty") then
+		yield("/echo ad start")
+		yield("/ad start")
+	end
