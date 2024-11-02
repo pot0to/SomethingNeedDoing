2d1125f9305eb607d181585200450a22ea099681
davoodinator <135515620+McVaxius@users.noreply.github.com>
2024-11-01 22:44:37 -0400

    its duct tape and it works
    its duct tape and it works
    iif i've PRed this is because i let it run over night and no chars were stuck :p

---
+++ Community Scripts/AutoRetainer Companions/AutoMaintenance/wall_dodger.lua
@@ -0,0 +1,55 @@
+--[[
+"move forwards a bit" after teleporting back to fc estate
+until lim fixes this problem with AR teleporting home to weird situation ;o
+loop wait for is char ready 
+
+setup automaton -> enhanced login -> run while AR is active. 
+/pcraft run moveforwardabit
+or whatever you called it.
+--]]
+
+function fartknocker()
+	while IsPlayerAvailable() == false do
+		yield("/echo waiting on player")
+		yield("/wait 1")
+	end
+end
+
+--don't run this on chars with blu level > 0. they might be actual play chars and it would be annoying to move randomly on login
+yield("/echo BLU -> "..tonumber(GetLevel(25)))
+
+isblu = tonumber(GetLevel(25))
+
+housing_zones = 
+{
+136, 	--mist
+340, 	--lavender beds
+341, 	--goblet
+641, 	--shirogane
+979 	--empyreum
+}
+
+if isblu == 0 then
+	zoyn = GetZoneID()
+	badzoyn = 1
+	for i=1,#housing_zones do
+		if housing_zones[i] == zoyn then
+			badzoyn = 0   --dont tp if we in a housing zone.
+		end
+	end
+	if badzoyn == 1 then
+		yield("/ays m d") --turn ar off for now
+		yield("/ays reset") --reset ar shenanigans for now
+
+		--teleport home
+		yield("/li fc")
+		fartknocker()
+		yield("/waitaddon NamePlate <maxwait.600><wait.5>")
+		yield("/wait 1")
+		yield("/hold W <wait.0.5>")
+		yield("/release W")
+
+		yield("/ays m e") --turn ar back on
+	end
+end
+
