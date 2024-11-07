c6fadfd102eb8789020fe6219e34e51ef3383a0d
davoodinator <135515620+McVaxius@users.noreply.github.com>
2024-11-07 11:59:15 -0500

    .
    .

--- Community Scripts/Dungeons/AutoDuty Helpers/Chest Uncucker.lua
+++ Community Scripts/Dungeons/AutoDuty Helpers/Chest Uncucker.lua
@@ -7,12 +7,15 @@ while cucked_by_chests == "often" do
 	--safe check ifs
 	if IsPlayerAvailable() then
 	if type(GetCharacterCondition(34)) == "boolean" and type(GetCharacterCondition(26)) == "boolean" and type(GetCharacterCondition(4)) == "boolean" then
-		--if GetCharacterCondition(26) == false and GetCharacterCondition(34) == true then --swap with next line if you just want looting while not in combat
-		if GetCharacterCondition(34) == true then
+		if GetCharacterCondition(26) == false and GetCharacterCondition(34) == true then
+		--if GetCharacterCondition(34) == true then
 			zist = GetDistanceToObject("Treasure Coffer")
 			if zist < 20 and zist > 0.1 then
+				yield("/ad pause")
 				yield("/vnavmesh moveto "..GetObjectRawXPos("Treasure Coffer").." "..GetObjectRawYPos("Treasure Coffer").." "..GetObjectRawZPos("Treasure Coffer"))
 				yield("/echo attempting to uncuck a chest....")
+				yield("/wait 5")
+				yield("/ad resume")
 			end
 		end
 	--safe check ends
