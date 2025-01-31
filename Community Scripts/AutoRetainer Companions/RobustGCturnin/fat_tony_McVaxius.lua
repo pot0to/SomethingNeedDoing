ffa6204306924ac98ab1cae41c76024e6db3474c
davoodinator <135515620+McVaxius@users.noreply.github.com>
2025-01-31 16:46:46 -0500

    tony get in there
    tony get in there

--- Community Scripts/AutoRetainer Companions/RobustGCturnin/fat_tony_McVaxius.lua
+++ Community Scripts/AutoRetainer Companions/RobustGCturnin/fat_tony_McVaxius.lua
@@ -189,18 +189,30 @@ for i=1,#franchise_owners do
 			yield("/echo See ya "..fat_tony..", a pleasure.")
 			yield("/wait 5")
 			CharacterSafeWait()
+			--[[
 			--added 5 second wait here because sometimes they get stuck.
 			yield("/wait 5")
+			yield("/tp Estate Hall")
+			yield("/wait 1")
+			--yield("/waitaddon Nowloading <maxwait.15>")
+			yield("/wait 15")
+			yield("/waitaddon NamePlate <maxwait.600><wait.5>")
+			--]]
+			--normal small house shenanigans
 			if franchise_owners[i][3] == 0 then
 				return_to_fc()			
+				CharacterSafeWait()
 				yield("/wait 5")
 				return_fc_entrance()
 			end
 			if franchise_owners[i][3] == 3 then --return to fc without returning to entrance
 				return_to_fc()			
+				CharacterSafeWait()
 			end
 			--retainer bell nearby shenanigans
 			if franchise_owners[i][3] == 1 then
+				return_to_fc()			
+				CharacterSafeWait()
 				yield("/target \"Summoning Bell\"")
 				yield("/wait 2")
 				PathfindAndMoveTo(GetObjectRawXPos("Summoning Bell"), GetObjectRawYPos("Summoning Bell"), GetObjectRawZPos("Summoning Bell"), false)
@@ -208,7 +220,6 @@ for i=1,#franchise_owners do
 			end
 			--limsa bell
 			if franchise_owners[i][3] == 2 then
-				yield("/echo returning to limsa bell")
 				return_to_limsa_bell()
 			end
 		end
